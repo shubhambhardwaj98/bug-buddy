9a85984ee683bddd4952b4c5f03ab780c55664a5
shubhambhardwaj <shubhamb957@gmail.com>
2022-04-07 04:24:35 +0530

    working on crud

--- bugbuddy/base/urls.py
+++ bugbuddy/base/urls.py
@@ -1,7 +1,9 @@
+from venv import create
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/', views.room, name="room")
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.createroom, name="create-room"),
 ]
--- bugbuddy/base/views.py
+++ bugbuddy/base/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render , redirect
+from .models import Room
+from .forms import RoomForm
+
+# rooms = [
+#     {'id': 1, 'name': 'lets learn python'},
+#     {'id': 2, 'name': 'design with me'},
+#     {'id': 3, 'name': 'frontend shit'},
+# ]
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
 
-
+def createroom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request,'base/room_form.html', context)
